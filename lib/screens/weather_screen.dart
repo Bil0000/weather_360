f28c6d6aff7f46c3cc8d6dc1baa42f6bc6a14b99
Bil0000 <bilal.bakr.elsherif@gmail.com>
2023-11-30 14:19:33 +0300

    upload to playstore

--- lib/screens/weather_screen.dart
+++ lib/screens/weather_screen.dart
@@ -709,7 +709,7 @@ class _WeatherPageState extends State<WeatherPage> with WidgetsBindingObserver {
                     const Text('Weather 360'),
                     SizedBox(height: 3),
                     Text(
-                      'Version: 6.0.0',
+                      'Version: 7.0.0',
                       style: TextStyle(
                         fontSize: 12,
                         color: Colors.grey,
