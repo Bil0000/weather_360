1e7575e9aff2796c904f102f8b50ab57fc2c2b29
Bil0000 <bilal.bakr.elsherif@gmail.com>
2023-12-12 12:23:38 +0300

    published bug fix to google play

--- lib/screens/weather_screen.dart
+++ lib/screens/weather_screen.dart
@@ -743,7 +743,7 @@ class _WeatherPageState extends State<WeatherPage> with WidgetsBindingObserver {
                         const Text('Weather 360'),
                         SizedBox(height: 3),
                         Text(
-                          'Version: 8.0.0',
+                          'Version: 8.0.1',
                           style: TextStyle(
                             fontSize: 12,
                             color: Colors.grey,
